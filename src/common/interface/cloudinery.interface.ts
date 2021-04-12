df6118e0f1625dd59b7147456488f88588ad473d
dolad <adavidoladele@gmail.com>
2021-04-12 21:04:08 +0100

    add

---
+++ src/common/interface/cloudinery.interface.ts
@@ -0,0 +1,11 @@
+export interface ICloudinary {
+  public_id: string;
+  version: number;
+  signature: string;
+  width: number;
+  height: number;
+  format: string;
+  resource_type: string;
+  url: string;
+  secure_url: string;
+}
